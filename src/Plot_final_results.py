bd58d591f5ad0f22423502878a0e7193f6eb66e7
bijanfallah <bijan.fallah@gmail.com>
2017-05-31 11:05:45 +0200

    Version May 31, with final Plots included

--- src/Plot_final_results.py
+++ src/Plot_final_results.py
@@ -1,23 +1,20 @@
-# run octave from python (IO code )
-# This code will run the IO code and produce the plots for RMSE and save the out put data
-# Created by Bijan Fallah 
-# info@bijan-fallah.com
-# -----------------------------------------------------------------------------------
-# DO not change any line here, it will be changed automatically by the io_scrpt.sh!!
-# -----------------------------------------------------------------------------------
-
-from oct2py import octave
+# This is a program to plot:
+# 1- maps of RMSEs for the ensemble analysis
+# 2- boxplot of RMSEs for Forecast vs Analysis
+# ------------------------- Imports ----------------------------
+import csv
+from itertools import izip
 import pandas as pd
 import csv
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
-from RMSE_MAPS_INGO import read_data_from_mistral as rdfm
-from CCLM_OUTS import Plot_CCLM
 import cartopy.crs as ccrs
-#from Plot_RMSE_SPREAD_main import plot_rmse_spread as prs
-# ============================================= NAMELIST ==========================================
+from CCLM_OUTS import Plot_CCLM
+from RMSE_MAPS_INGO import read_data_from_mistral as rdfm
 
+# --------------------------------------------------------------
+# ------------------------- Namelist ---------------------------
 SEAS='DJF'
 NN=1000#number of observations should be read from previous funcions!!!!
 month_length=20
@@ -27,39 +24,36 @@ buffer=20
 timesteps=10   # number of the seasons (years)
 start_time=0
 name_2 = 'member_relax_3_big_00_' + Vari + '_ts_splitseas_1990_1999_' + SEAS + '.nc'
-member=0
 DIR='/home/fallah/Documents/DATA_ASSIMILATION/Bijan/CODES/Optimal_Interpolation/optiminterp/inst/'
 DIR_exp="/home/fallah/Documents/DATA_ASSIMILATION/Bijan/CODES/CCLM/Python_Codes/historical_runs_yearly_ensemble/src/"
-
-# =================================================================================================
-octave.run(DIR+"run_IO.m") # Running the Octave Interface from python!
-# -------------------------------------------------------------------------------------------------
-
-
-LAT = pd.read_csv(DIR_exp+"Trash/LAT.csv", header=None)
-LON = pd.read_csv(DIR_exp+"Trash/LON.csv", header=None)
-Forecast_3 = np.array(pd.read_csv(DIR_exp+'Trash/SEASON_MEAN1' + '_' + SEAS + '.csv', header=None))#Reading the Forecast values
+no_members=20
+inflation=1.1 #NEW variable!!!!
+# --------------------------------------------------------------
+LAT = pd.read_csv(DIR_exp+'NAMES'+'/'+"Trash/LAT.csv", header=None)
+LON = pd.read_csv(DIR_exp+'NAMES'+'/'+"Trash/LON.csv", header=None)
+Forecast_3 = np.array(pd.read_csv(DIR_exp+'NAMES'+'/'+'Trash/SEASON_MEAN1' + '_' + SEAS + '.csv', header=None))#Reading the Forecast values
 t_f = np.zeros((month_length,Forecast_3.shape[0],Forecast_3.shape[1]))
 for month in range(0, month_length):# Reading the ensemble forecast for each month!
-    t_f[month,:,:] = pd.read_csv(DIR_exp+'Trash/SEASON_MEAN' + str(month) + '_' + SEAS + '.csv', header=None)
+    t_f[month,:,:] = pd.read_csv(DIR_exp+'NAMES'+'/'+'Trash/SEASON_MEAN' + str(month) + '_' + SEAS + '.csv', header=None)
 t_f = np.array(t_f)
-
 ## add correction to forecast :
 # declare zero matrix which will be filled
 result_IO = np.zeros((month_length,Forecast_3.shape[0],Forecast_3.shape[1]))
-result = np.zeros((Forecast_3.shape[0],Forecast_3.shape[1]))
+
 for i in range(0,month_length):
-    fil=DIR + 'fi' + str(member) + str(i) +'.csv'
-    result=np.array(list(csv.reader(open(fil,"rb"),delimiter=','))).astype('float')
-    result_IO[i,:,:] = np.squeeze(t_f[i,:,:]) + result
+    result = np.zeros((Forecast_3.shape[0], Forecast_3.shape[1]))
+    for member in range(0,no_members):
+        fil='333333'+ str(member)+'_' + str(inflation)+'/optiminterp/inst/' + 'fi' + str(member) + str(i) +'.csv'
+        result=result + np.array(list(csv.reader(open(fil,"rb"),delimiter=','))).astype('float')
+        
+    result_IO[i,:,:] = np.squeeze(t_f[i,:,:]) + (result/no_members)
+   
+# plot the ensemble Analysis RMSE :
 
-
-# plot differences
-
-pdf_name= 'last_m100_l20_'+str(member)+'.pdf'
-t_o, lat_o, lon_o, rlat_o, rlon_o =rdfm(dir='/work/bb1029/b324045/work5/03/member_relax_3_big_00/post/', # the observation (default run without shifting)
-                                            name=name_2,
-                                            var=Vari)
+pdf_name='Ensemble_RMSE_last_m100_l20.pdf'
+t_o, lat_o, lon_o, rlat_o, rlon_o =rdfm(dir='/work/bb1029/b324045/work5/03/member_relax_3_big_00/post/',
+                                        name=name_2,
+                                        var=Vari)
 dext_lon = t_o.shape[2] - (2 * buffer)
 dext_lat = t_o.shape[1] - (2 * buffer)
 start_lon=(buffer+4)
@@ -74,29 +68,26 @@ for i in range(0,forecast.shape[1]):
         forecast_resh=np.squeeze(forecast[:,i,j])
         obs_resh=np.squeeze(obs[:,i,j])
         RMSE[i,j] = mean_squared_error(obs_resh, forecast_resh) ** 0.5 # Calculating the RMSEs for each grid point
-        
-        
+
 for i in range(0,forecast.shape[0]):
     forecast_resh_ts=np.squeeze(forecast[i,:,:])
     obs_resh_ts=np.squeeze(obs[i,:,:])
     RMSE_TIME_SERIES[i] = mean_squared_error(obs_resh_ts, forecast_resh_ts) ** 0.5 #Calculating RMSEs for each month for Analysis
 
+
 for i in range(0,forecast.shape[0]):
     forecast_orig_ts=np.squeeze(t_f[i,:,:])
     obs_resh_ts=np.squeeze(obs[i,:,:])
     RMSE_TIME_SERIES_Forecast[i] = mean_squared_error(obs_resh_ts, forecast_orig_ts) ** 0.5 #Calculating RMSEs for each month for forecast
-
+## Plotting core :
 
 fig = plt.figure('1')
 fig.set_size_inches(14, 10)
-
 
 rp = ccrs.RotatedPole(pole_longitude=-165.0,# this number comes from int2clm settings
                           pole_latitude=46.0,# this number comes from int2clm settings
                           globe=ccrs.Globe(semimajor_axis=6370000,
                                            semiminor_axis=6370000))
-
-
 pc = ccrs.PlateCarree()
 ax = plt.axes(projection=rp)
 ax.coastlines('50m', linewidth=0.8)
@@ -105,15 +96,13 @@ if SEAS[0] == "D":
 else:
     v = np.linspace(0, .8, 9, endpoint=True)# the limits of the colorbar for other seasons
 
-
 # Write the RMSE mean of the member in a file
-import csv
-from itertools import izip
-names=DIR_exp+'Trash/'+'RMSE_'+pdf_name+str(member)+'.csv'
+
+names=DIR_exp+'RMSE_'+pdf_name+'.csv'
 with open(names, 'wb') as f:
      writer = csv.writer(f)
      writer.writerow([NN,np.mean(RMSE)])
-  
+     
 lats_f1=lat_o[buffer:buffer + dext_lat, buffer:buffer + dext_lon]
 lons_f1=lon_o[buffer:buffer + dext_lat, buffer:buffer + dext_lon]
 cs=plt.contourf(lons_f1, lats_f1, RMSE,v, transform=ccrs.PlateCarree(), cmap=plt.cm.terrain)
@@ -133,7 +122,7 @@ ax.text(-19.83, -35.69, r'$0\degree $',
         fontsize=15)
 ax.text(15.106, -35.69, r'$20\degree E$',
         fontsize=15)
-
+        
 plt.hlines(y=min(rlat_o), xmin=min(rlon_o), xmax=max(rlon_o), color='black',linestyles= 'dashed', linewidth=2)
 plt.hlines(y=max(rlat_o), xmin=min(rlon_o), xmax=max(rlon_o), color='black',linestyles= 'dashed', linewidth=2)
 plt.vlines(x=min(rlon_o), ymin=min(rlat_o), ymax=max(rlat_o), color='black',linestyles= 'dashed', linewidth=2)
@@ -142,41 +131,31 @@ plt.hlines(y=min(rlat_o[buffer:-buffer]), xmin=min(rlon_o[buffer:-buffer]), xmax
 plt.hlines(y=max(rlat_o[buffer:-buffer]), xmin=min(rlon_o[buffer:-buffer]), xmax=max(rlon_o[buffer:-buffer]), color='black', linewidth=4)
 plt.vlines(x=min(rlon_o[buffer:-buffer]), ymin=min(rlat_o[buffer:-buffer]), ymax=max(rlat_o[buffer:-buffer]), color='black', linewidth=4)
 plt.vlines(x=max(rlon_o[buffer:-buffer]), ymin=min(rlat_o[buffer:-buffer]), ymax=max(rlat_o[buffer:-buffer]), color='black', linewidth=4)
-
 Plot_CCLM(dir_mistral='/work/bb1029/b324045/work5/03/member_relax_3_big_00/post/',name=name_2,bcolor='black',var=Vari,flag='FALSE',color_map='TRUE', alph=1, grids='FALSE', grids_color='red', rand_obs='TRUE', NN=NN)
-
 xs, ys, zs = rp.transform_points(pc,
                                  np.array([-17, 105.0]),# Adjust for other domains!
                                  np.array([3, 60])).T   # Adjust for other domains!
-
 ax.set_xlim(xs)
 ax.set_ylim(ys)
-
-#prs(PDF=DIR_exp+'Trash/'+pdf_name,vari="RMSE",VAL=RMSE,x=forecast.shape[1],y=forecast.shape[2])
-
-
-
-plt.savefig(DIR_exp+'Trash/'+pdf_name)
+plt.savefig(DIR_exp+pdf_name)
 plt.close()
 
 # RMSE time-series
 
 fig = plt.figure('2')
 fig.set_size_inches(14, 10)
-#plt.plot(RMSE_TIME_SERIES, 'o-', c='green')
-#plt.xlabel('$time$', size=35)
 plt.ylabel('$RMSE$', size=35)
 plt.title('Boxplot of seasonal RMSEs averaged over the domain', size=30 , y=1.02)
 #plt.ylim([0,.45])
 
-names=DIR_exp+'Trash/'+pdf_name+str(member)+'_Analysis.csv'
+names=DIR_exp+pdf_name+'_Analysis.csv'
 with open(names, 'wb') as f:
      writer = csv.writer(f)
      writer.writerow(RMSE_TIME_SERIES)
-#names_1 = '/home/fallah/Documents/DATA_ASSIMILATION/Bijan/CODES/CCLM/Python_Codes/historical_runs_yearly/src/TEMP/Figure08_RMSE_T_2M.pdf_Forecast.csv'
-#fore = np.array(list(csv.reader(open(names_1,"rb"),delimiter=','))).astype('float')
+
 plt.boxplot([RMSE_TIME_SERIES,RMSE_TIME_SERIES_Forecast.transpose(), ])
 plt.xticks([1, 2], ['$Analysis$', '$Forecast$'], size=35)
-#plt.ylim([0,.45])
-plt.savefig(DIR_exp+'Trash/'+pdf_name + str(member)+'_ts.pdf')
+plt.savefig(DIR_exp+pdf_name+'_ts.pdf')
 plt.close()
+
+
---
+++ src/io_scrpt.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+# ======================================================================================================================
+# .______    __         __       ___      .__   __.     _______    ___       __       __          ___       __    __
+# |   _  \  |  |       |  |     /   \     |  \ |  |    |   ____|  /   \     |  |     |  |        /   \     |  |  |  |
+# |  |_)  | |  |       |  |    /  ^  \    |   \|  |    |  |__    /  ^  \    |  |     |  |       /  ^  \    |  |__|  |
+# |   _  <  |  | .--.  |  |   /  /_\  \   |  . `  |    |   __|  /  /_\  \   |  |     |  |      /  /_\  \   |   __   |
+# |  |_)  | |  | |  `--'  |  /  _____  \  |  |\   |    |  |    /  _____  \  |  `----.|  `----./  _____  \  |  |  |  |
+# |______/  |__|  \______/  /__/     \__\ |__| \__|    |__|   /__/     \__\ |_______||_______/__/     \__\ |__|  |__|
+#
+#                                                info@bijan-fallah.com
+# ======================================================================================================================
+# TODO: plot the time-series of ensemble members for seasonal values
+# TODO: check the effect of the ensemble number on the RMSE
+# TODO: check the effect of inflation!
+# TODO: Plot the final RMSE maps and SPREAD boxplot forecast vs analysis
+
+
+
+set -ex
+# ====================================== NAMELIST ======================================================================
+month_length=10
+SEAS="JJA"
+NN=1000
+Var='T_2M'
+#Var='TOT_PREC'
+#COR_LEN=1
+M=50 #Number of influential points
+DIR_python='/home/fallah/Documents/DATA_ASSIMILATION/Bijan/CODES/CCLM/Python_Codes/historical_runs_yearly_ensemble/src'
+DIR_OI='/home/fallah/Documents/DATA_ASSIMILATION/Bijan/CODES/Optimal_Interpolation/'
+no_members=20
+buffer=20
+inflation=1.1
+DIR_WORK='/scratch/users/fallah/exp01/'
+# ================================================================================================
+if [ ! -d "${DIR_WORK}${inflation}" ]; then
+  mkdir ${DIR_WORK}${inflation}
+fi
+DIR_WORK=${DIR_WORK}/${inflation}/
+while [ $NN -lt 1001 ]; do
+ COR_LEN=3
+ while [ $COR_LEN -lt 4 ]; do
+     member=0
+     while [ $member -lt $no_members ]; do
+
+         NAME='Yearly_RUN'
+         NAME_it=${NAME}_${COR_LEN}_${NN}_
+         NAME=${NAME}_${COR_LEN}_${NN}_${member}_${inflation}
+
+         if [ ! -d "${DIR_WORK}${NAME}" ]; then
+          mkdir ${DIR_WORK}${NAME}
+         fi
+         cp ${DIR_python}/*py ${DIR_WORK}${NAME}/
+         cp ${DIR_python}/Plot_final_results.py ${DIR_WORK}/
+         cp ${DIR_python}/CCLM_OUTS.py ${DIR_WORK}/
+         cp ${DIR_python}/RMSE_MAPS_INGO.py ${DIR_WORK}/
+         cp ${DIR_python}/rotgrid.py ${DIR_WORK}/
+         cp -rf ${DIR_OI}optiminterp ${DIR_WORK}${NAME}/
+         XX="$DIR_WORK$NAME/Stations$NAME"
+
+         # ===========================================   MAKE PSEUDO  ======================================================
+         sed -i "s/NN=600/NN=$NN/g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         echo $SEAS
+         sed -i "s/DJF/$SEAS/g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         sed -i "s/month_length=20/month_length=$month_length/g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         sed -i "s/buffer=20/buffer=$buffer/g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         sed -i "s/NO=20/NO=$no_members/g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         var1=$(echo ${DIR_python})
+         var2=$(echo ${DIR_WORK}${NAME})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         # ========================================== END MEKE PSEUDO ======================================================
+
+         # ======================================= Create INPUT FILES ======================================================
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         sed -i "s/NN=1000#number/NN=$NN#number/g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         sed -i "s/month_length=20/month_length=$month_length/g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         sed -i "s/"DJF"/"$SEAS"/g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         sed -i "s/members=20/members=$no_members/g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         var1=$(echo ${DIR_python})
+         var2=$(echo ${DIR_WORK}${NAME})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         sed -i "s/inflation=1.0/inflation=$inflation/g" ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         # ====================================  END Create INPUT FILES  ===================================================
+
+
+         #sed -i "s/NN=500);/NN=$NN);/g" ${DIR_WORK}/${NAME}/CCLM_OUTS.py
+         sed -i "s/'RMSE_Patterns_'/'RMSE_Patterns_$NAME'/g" ${DIR_WORK}${NAME}/RMSE_MAPS_INGO.py
+
+         sed -i "s/month_length=20/month_length=$month_length/g" ${DIR_WORK}${NAME}/RMSE_MAPS_INGO.py
+         sed -i "s/"DJF"/"$SEAS"/g" ${DIR_WORK}${NAME}/RMSE_MAPS_INGO.py
+
+         sed -i "s/Stations/Stations$NAME/g" ${DIR_WORK}${NAME}/PLOT_Stations.py
+
+
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}${NAME}/PLOT_Stations.py
+
+
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}${NAME}/run_octave.py
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}${NAME}/RMSE_MAPS_INGO.py
+
+         if [ ${Var} = 'TOT_PREC' ];then
+            sed -i "s/[K]/mm/g" ${DIR_WORK}${NAME}/RMSE_MAPS_INGO.py
+
+         fi
+
+
+         sed -i "s/NN=1000/NN=$NN/g" ${DIR_WORK}${NAME}/PLOT_Stations.py
+         sed -i "s/DJF/$SEAS/g" ${DIR_WORK}${NAME}/run_octave.py
+         sed -i "s/NN=1000#number/NN=$NN#number/g" ${DIR_WORK}${NAME}/run_octave.py
+         sed -i "s/month_length=20/month_length=$month_length/g" ${DIR_WORK}${NAME}/run_octave.py
+         var1=$(echo ${DIR_OI}optiminterp/inst/)
+         var2=$(echo ${DIR_WORK}${NAME}/optiminterp/inst/)
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}${NAME}/run_octave.py
+
+         var1=$(echo ${DIR_python})
+         var2=$(echo ${DIR_WORK}${NAME})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}${NAME}/run_octave.py
+
+         sed -i "s/member=0/member=$member/g" ${DIR_WORK}${NAME}/run_octave.py
+         sed -i "s/'last_m100_l20/'RMSE_ANALYSIS_${SEAS}_${M}_${COR_LEN}_${NN}_${Var}/g" ${DIR_WORK}${NAME}/run_octave.py
+         sed -i "s/buffer=20/buffer=$buffer/g" ${DIR_WORK}${NAME}/run_octave.py
+
+
+         # ------------------------------------------  Plot_final_results.py -------------------------------------------
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/DJF/$SEAS/g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/NN=1000#number/NN=$NN#number/g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/month_length=20/month_length=$month_length/g" ${DIR_WORK}/Plot_final_results.py
+         var1=$(echo ${DIR_OI}optiminterp/inst/)
+         var2=$(echo ${DIR_WORK}/optiminterp/inst/)
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}/Plot_final_results.py
+         var1=$(echo ${DIR_python})
+         var2=$(echo ${DIR_WORK})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/no_members=20/no_members=$no_members/g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/'last_m100_l20/'RMSE_ANALYSIS_${SEAS}_${M}_${COR_LEN}_${NN}_${Var}/g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/buffer=20/buffer=$buffer/g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/NAMES/${NAME}/g" ${DIR_WORK}/Plot_final_results.py
+         n="333333"
+         var1=$(echo ${n})
+         var2=$(echo ${DIR_WORK}${NAME_it})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}/Plot_final_results.py
+         sed -i "s/inflation=1.1/inflation=$inflation/g" ${DIR_WORK}/Plot_final_results.py
+         #------------------------------------------- run_IO.m -------------------------------------------------------------
+         sed -i "s/lenx = 20;/lenx = $COR_LEN/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         sed -i "s/leny = 20;/leny = $COR_LEN/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         sed -i "s/m    = 50/m    = $M/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         var1=$(echo ${DIR_python})
+         var2=$(echo ${DIR_WORK}${NAME})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         month=`expr $month_length - 1`
+         sed -i "s/i=0:11/i=0:$month/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         #sed -i "s/historical_runs/historical_runs_yearly_ensemble/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         sed -i "s/INPUT.csv/INPUT${member}.csv/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         sed -i "s/'fi'/'fi${member}'/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+         sed -i "s/'vari'/'vari${member}'/g" ${DIR_WORK}${NAME}/optiminterp/inst/run_IO.m
+
+         # ====================================    Plot_RMSE_SPREAD_main.py ============================================
+         sed -i "s/T_2M/$Var/g" ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+         sed -i "s/NN=1000#number/NN=$NN#number/g" ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+         sed -i "s/timesteps=10/timesteps=$month_length/g" ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+         sed -i "s/DJF/$SEAS/g" ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+         sed -i "s/buffer=20/buffer=$buffer/g" ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+
+         var1=$(echo ${DIR_python})
+         var2=$(echo ${DIR_WORK}${NAME})
+         sed -i "s%$var1%$var2%g" ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+
+
+
+         # ===============================================================================================
+         # ==============================  Python calls: =================================================
+
+         python ${DIR_WORK}${NAME}/PLOT_Stations.py
+         python ${DIR_WORK}${NAME}/Plot_RMSE_SPREAD_main.py
+         python ${DIR_WORK}${NAME}/make_pseudo_obs.py
+         python ${DIR_WORK}${NAME}/Create_Input_FIles.py
+         python ${DIR_WORK}${NAME}/run_octave.py
+
+         # ===============================================================================================
+         
+
+         member=`expr $member + 1`
+     done
+     python ${DIR_WORK}${NAME}/Plot_final_results.py # Plot final results
+     COR_LEN=`expr $COR_LEN + 1`
+     echo ${COR_LEN}
+ done
+
+ NN=`expr $NN + 100`
+ echo $NN
+done
+
+
+
+
+
